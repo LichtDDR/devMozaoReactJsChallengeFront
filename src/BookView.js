524cdc413c071ec1eea29c75d814c9382ed6b4e2
Mikael Scientia <mikaelmuniz2001@gmail.com>
2021-06-13 18:26:12 -0300

    deleting

--- src/BookView.js
+++
@@ -1,9 +0,0 @@
-import React from 'React';
-const query = 'harry potter';
-
-fetch("https://www.googleapis.com/books/v1/volumes?q=${query}")
-const BookView = () => {
-    return (
-        <h4>Discover a new book</h4>
-    );
-}
