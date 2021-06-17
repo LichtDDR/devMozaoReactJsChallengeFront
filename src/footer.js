fa9094f288707021a7f9d30564f9da35553760c8
Mikael Scientia <mikaelmuniz2001@gmail.com>
2021-06-17 11:53:51 -0300

    updated

--- src/footer.js
+++ src/footer.js
@@ -6,6 +6,10 @@ const Footer = () => {
     return (
         <div className="footer">
             <h1>Contato</h1>
+            <h3>Teste</h3>
+            <div className="rights">
+                <h3>Direitos reservados à Mikael 2021</h3>
+            </div>
         </div>
     );
 }
